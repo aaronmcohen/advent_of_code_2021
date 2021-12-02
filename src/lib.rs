6192cbb4a25f7c931417fc9f5236f94c8a7e6332
Aaron Cohen <amcohen@us.ibm.com>
2021-12-02 15:10:39 -0500

    Completed day 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 mod day1;
+mod day2;
 
 #[cfg(test)]
 mod tests {
     use crate::day1::count_increase;
     use crate::day1::count_increase_sums;
+    use crate::day2::get_horizontal_and_vertical;
+    use crate::day2::get_horizontal_and_vertical_and_aim;
     #[test]
     fn day1_puzzle1() {
         assert_eq!(1713, count_increase());
@@ -12,4 +15,20 @@ mod tests {
     fn sliding_window() {
        assert_eq!(1734, count_increase_sums()); 
     }
+    #[test]
+    fn calculate_horizontal_vertical(){
+        let (h,v) = get_horizontal_and_vertical();
+        println!("h{}",h);
+        println!("v{}",v);
+        assert_eq!(1670340,h*v);
+    }
+    #[test]
+    fn calculate_horizontal_vertical_aim(){
+        let (h,v,a) = get_horizontal_and_vertical_and_aim();
+        println!("h{}",h);
+        println!("v{}",v);
+        println!("a{}",a);
+        assert_eq!(1954293920,h*v);
+    }
+
 }
